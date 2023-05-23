f41b68acc3be631bdf0f17f019f4cfd98680825c
MGibadulin <gibadulin.m.a@gmail.com>
2023-05-23 11:58:47 +0200

    Change table audit_process_log

--- meta_ds/create_tables_meta.sql
+++ meta_ds/create_tables_meta.sql
@@ -1,13 +1,14 @@
 --Create table for audit
 CREATE TABLE IF NOT EXISTS `paid-project-346208`.`meta_ds`.`audit_process_log`
 (
-	id			STRING NOT NULL,
-	process		STRING NOT NULL,
-	start_ts	TIMESTAMP NOT NULL,
-	end_ts		TIMESTAMP,
-	truncated 	INT64,
-	inserted	INT64,
-	updated 	INT64,
+	process_log_id	STRING NOT NULL,
+	process			STRING NOT NULL,
+	start_ts		TIMESTAMP NOT NULL,
+	end_ts			TIMESTAMP,
+	truncated 		INT64,
+	inserted		INT64,
+	updated 		INT64,
 	mark_as_deleted INT64,
-	message 	STRING
+	message 		STRING,
+	owner_by 		STRING
 );
