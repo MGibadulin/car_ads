5efccae3e219fc1589ceee20a247b094f83cadab
MGibadulin <gibadulin.m.a@gmail.com>
2023-06-01 11:48:43 +0200

    Fix type_action to upper

--- meta_ds/procedures_meta.sql
+++ meta_ds/procedures_meta.sql
@@ -27,13 +27,13 @@ CREATE OR REPLACE PROCEDURE `paid-project-346208`.`meta_ds`.`usp_write_process_l
 	IN metrics STRUCT <truncated INT64, inserted INT64, updated INT64, mark_as_deleted INT64, message STRING>
 )
 BEGIN
-	IF type_action = "START"
+	IF UPPER(type_action) = "START"
 		THEN
 			SET task_id = GENERATE_UUID();
 			INSERT INTO `paid-project-346208`.`meta_ds`.`audit_process_log`(process_log_id, process, start_ts, owner_by)
 			VALUES(task_id, process_name, CURRENT_TIMESTAMP(), SESSION_USER());
 	END IF;
-    IF type_action = "END"
+    IF UPPER(type_action) = "END"
         THEN
             UPDATE `paid-project-346208`.`meta_ds`.`audit_process_log`
                 SET end_ts = CURRENT_TIMESTAMP(),
