cbb4a697bdb13de2ed79667d054df07a4a129210
MGibadulin <gibadulin.m.a@gmail.com>
2023-08-15 21:46:50 +0500

    Fix SQL

--- landing/MSSQL/create_tables_lnd.sql
+++ landing/MSSQL/create_tables_lnd.sql
@@ -22,7 +22,7 @@ CREATE TABLE Landing.dbo.lnd_ads_archive (
 	ads_id int NOT NULL,
 	source_id nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	card_url nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	modify_date datetime DEFAULT getdate() NOT NULL,
+	modify_date datetime NOT NULL,
 	card_compressed varbinary(MAX) NULL,
 	CONSTRAINT PK_ads_arch_ads_id PRIMARY KEY (ads_id,modify_date,source_num)
 );
