1f83598b8c02d032b6fbe938ba9bc31d3af76842
MGibadulin <gibadulin.m.a@gmail.com>
2023-08-16 14:44:32 +0200

    Fix comment

--- landing/MSSQL/create_tables_lnd.sql
+++ landing/MSSQL/create_tables_lnd.sql
@@ -23,6 +23,6 @@ CREATE TABLE Landing.dbo.lnd_ads_archive (
 	source_id nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	card_url nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	card_compressed varbinary(MAX) NULL,
---	process_log_id int NOT NULL,
+	process_log_id int NOT NULL,
 	insert_date datetime DEFAULT getdate() NOT NULL
 );
