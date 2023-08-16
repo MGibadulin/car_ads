2b1af8037c78c0544c7d5b6a76f3d15ed5b802c4
MGibadulin <gibadulin.m.a@gmail.com>
2023-08-16 14:45:03 +0200

    Update

--- landing/MSSQL/fill_load.py
+++ landing/MSSQL/fill_load.py
@@ -30,7 +30,7 @@ def download_cards_from_source(connection, batch_size: int, ads_id: int, process
                 where ad_status = 2
                 and ads_id between {ads_id} and {ads_id+batch_size-1}
                 and modify_date < '{process_start_time}';"""
-    cursor = connection.cursor() # as_dict=True
+    cursor = connection.cursor(as_dict=True)
     cards = []
     try:
         cursor.execute(stmt)
@@ -39,15 +39,20 @@ def download_cards_from_source(connection, batch_size: int, ads_id: int, process
         print("Caught a pymssql.Error exception:", err)
     return cards
 
-def upload_cards_to_destanation(connection, cards):
+def upload_cards_to_destanation(connection, process_log_id, cards):
     """Upload cards to target DB."""
     stmt = r"""insert into [Landing].[dbo].[lnd_ads_archive] (
                      ads_id
                     ,source_id
                     ,card_url
                     ,card_compressed
+                    ,process_log_id
                     ) values """
-    stmt += ", ".join(fr"{card}" for card in cards)
+    stmt += ", ".join(f"""( {card['ads_id']}, 
+                      '{card['source_id']}', 
+                      '{card['card_url']}',  
+                      CONVERT(VARBINARY(MAX), '0x'+'{card['card_compressed'].hex()}', 1), 
+                      {process_log_id})""" for card in cards)
     cursor = connection.cursor()
     
     try:
@@ -55,9 +60,50 @@ def upload_cards_to_destanation(connection, cards):
     except  pymssql.Error as err:
         print("Caught a pymssql.Error exception:", err)
     
+def write_process_log(cursor, action):
+    """Write process log."""
+    
+    process_log_id = None
+    if action == "START":
+        cursor.execute(f"""
+                    insert into [Landing].[dbo].[process_log] (process_desc, [user], host, connection_id)         
+                    select '{PROCESS_DESC}', 
+                            SYSTEM_USER, 
+                            HOST_NAME(),
+                            @@spid;
+                """)
+        cursor.execute("select scope_identity() as process_log_id;")
+        process_log_id = cursor.fetchone()[0]
+    elif action == "END":
+        cursor.execute(
+            f"""
+                update process_log 
+                    set end_date = getdate() 
+                where process_log_id = {process_log_id};
+            """
+        )
+    return process_log_id
+
+def get_process_start_time(cursor, process_log_id):
+    """Get process start time."""
+    
+    cursor.execute(f"select start_date from [Landing].[dbo].[process_log] where process_log_id = {process_log_id}")
+    process_start_time = str(cursor.fetchone()[0])[:-3]
+    return process_start_time
+
+def get_max_ads_id(cursor, process_start_time):
+    """Get max ads_id."""
+    
+    stmt =  f"""select
+                    max(ads_id) from [car_ads_training_db].[dbo].[ads_archive] 
+                    where ad_status=2
+                    and modify_date < '{process_start_time}';"""
+    cursor.execute(stmt)
+    max_ads_id = cursor.fetchone()[0]
+    return max_ads_id
 
 def main():
-    """Main fiction."""
+    """Main fuction."""
 
     configs = get_config()
 
@@ -68,31 +114,18 @@ def main():
     print(f"{time.strftime('%X', time.gmtime())}, Connected to target database")
 
     with source_db_conx, target_db_conx:
+        
         source_cur = source_db_conx.cursor()
         target_cur = target_db_conx.cursor()
 
-        target_cur.execute(f"""
-                insert into [Landing].[dbo].[process_log] (process_desc, [user], host, connection_id)         
-                select '{PROCESS_DESC}', 
-                       SYSTEM_USER, 
-                       HOST_NAME(),
-                       @@spid;
-            """)
-
-        target_cur.execute("select scope_identity() as process_log_id;")
-        process_log_id = target_cur.fetchone()[0]
-
-        source_cur.execute("select getdate()")
-        process_start_time = str(source_cur.fetchone()[0])[:-3]
-
-        stmt =  f"""select
-                    max(ads_id) from [car_ads_training_db].[dbo].[ads_archive] 
-                    where ad_status=2
-                    and modify_date < '{process_start_time}';"""
-        source_cur.execute(stmt)
+        process_log_id = write_process_log(target_cur, "START")
+        
+        # ! time from source may be difference from target
+        process_start_time = get_process_start_time(target_cur, process_log_id)
+        
+        max_ads_id = get_max_ads_id(source_cur, process_start_time)
+        
         print(f"{time.strftime('%X', time.gmtime())}, Get max ads_id from source database")
-
-        max_ads_id = source_cur.fetchone()[0]
 
         for batch_num, ads_id in enumerate(range(1, max_ads_id + 1, configs["batch_size"])):
 
@@ -103,16 +136,10 @@ def main():
 
             print(f"{time.strftime('%X', time.gmtime())}. Download cards. Batch #{batch_num}.")
 
-            upload_cards_to_destanation(target_db_conx, cards)
+            upload_cards_to_destanation(target_db_conx, process_log_id, cards)
             print(f"{time.strftime('%X', time.gmtime())}. Upload cards to destanation. Batch #{batch_num}.")
 
-        target_cur.execute(
-            f"""
-                update process_log 
-                    set end_date = getdate() 
-                where process_log_id = {process_log_id};
-            """
-        )
+        write_process_log(target_cur, "END")
 
 if __name__ == "__main__":
     main()
