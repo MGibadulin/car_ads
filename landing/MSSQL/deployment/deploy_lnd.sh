73243db2d2bcfa1562a5ebe635ee502671eb1895
MGibadulin <gibadulin.m.a@gmail.com>
2023-08-28 22:41:49 +0500

    Update

--- landing/MSSQL/deployment/deploy_lnd.sh
+++ landing/MSSQL/deployment/deploy_lnd.sh
@@ -4,12 +4,12 @@ echo "------------------------------------------------------------"
 echo "Create tables"
 echo "------------------------------------------------------------"
 
-LIST = ./DDL/tables/process_log.sql ./DDL/tables/event_log.sql ./DDL/tables/ads_archive.sql
+LIST=("../DDL/tables/process_log.sql" "../DDL/tables/event_log.sql" "../DDL/tables/lnd_ads_archive.sql")
 
-for ITEM in $LIST
+for ITEM in ${LIST[@]}
 do
     echo "Create table $ITEM"
-    sqlcmd -S localhost -U sa -P pass -i $ITEM
+    sqlcmd -S "server_ip,1434" -U sa -P pass -i $ITEM
     wait
     echo "Done"
 done
@@ -35,18 +35,18 @@ pkill -f scraper.py
 
 echo "------------------------------------------------------------"
 echo Starting full_load.py
-python3 /path_to_script/full_load.py &
+python3 ../full_load.py &
 echo Full load running in background...
 wait
 
 echo "------------------------------------------------------------"
 echo Testing full load
-echo Run SQL script get number rows from source_db.ads_archive
-RESULT_1 = $(sqlcmd -S localhost -U sa -P pass -h -1 -Q "select count(*) from source_db.ads_archive;")
+echo Run SQL script get number rows from car_ads_training_db.dbo.ads_archive
+RESULT_1 = $(sqlcmd -S "server_ip,1434" -U sa -P pass -h -1 -Q "select count(*) from car_ads_training_db.dbo.ads_archive;")
 wait
 
-echo Run SQL script get number rows from landing.ads_archive
-RESULT_2 = $(sqlcmd -S localhost -U sa -P pass -h -1 -Q "select count(*) from landing.ads_archive;")
+echo Run SQL script get number rows from Landing.dbo.lnd_ads_archive
+RESULT_2 = $(sqlcmd -S "server_ip,1434" -U sa -P pass -h -1 -Q "select count(*) from Landing.dbo.lnd_ads_archive;")
 wait
 echo Compare number of rows
 if [ $RESULT_1 -eq $RESULT_2 ]
@@ -73,7 +73,7 @@ pkill -f scrapper.py
 
 echo "------------------------------------------------------------"
 echo Starting incremental_load.py
-python3 /path_to_script/incremental_load.py &
+python3 ../incremental_load.py &
 echo Incremental load running in background...
 wait
 
