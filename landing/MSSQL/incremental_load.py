cd69d555850f2a6ee429bb2f3efe0a3a4bfc45ea
MGibadulin <gibadulin.m.a@gmail.com>
2023-08-30 15:06:31 +0200

    Fix name table in source

--- landing/MSSQL/incremental_load.py
+++ landing/MSSQL/incremental_load.py
@@ -46,7 +46,7 @@ def extract_data_from_source(connection, batch_size: int, start_ads_id: int, pro
             card_url,
             card_compressed,
             source_num
-        from [Landing].[dbo].[ads_archive]
+        from [car_ads_training_db].[dbo].[ads_archive]
         where ads_id between {start_ads_id} and {start_ads_id+batch_size-1}
             and modify_date < '{process_start_time}'
             and modify_date >= '{previous_load_time}';"""
@@ -155,7 +155,7 @@ def get_max_ads_id(cursor, process_start_time, previous_load_time):
     """Get max ads_id."""
     
     stmt =  f"""select
-                    max(ads_id) from [Landing].[dbo].[ads_archive] 
+                    max(ads_id) from [car_ads_training_db].[dbo].[ads_archive] 
                     where modify_date < '{process_start_time}'
                     and modify_date >= '{previous_load_time}';"""
     try:
