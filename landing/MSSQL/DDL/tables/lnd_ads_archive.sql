d4f0a5de9ef478d0349c910b1eecd1c5c19c6404
MGibadulin <gibadulin.m.a@gmail.com>
2023-09-01 08:24:24 +0200

    Add  column source_date

--- landing/MSSQL/DDL/tables/lnd_ads_archive.sql
+++ landing/MSSQL/DDL/tables/lnd_ads_archive.sql
@@ -7,5 +7,6 @@ CREATE TABLE Landing.dbo.lnd_ads_archive (
 	card_url nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	card_compressed varbinary(MAX) NULL,
 	process_log_id int NOT NULL,
+	source_date datetime NOT NULL,
 	insert_date datetime DEFAULT getdate() NOT NULL
 );
